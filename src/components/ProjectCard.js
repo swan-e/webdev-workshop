0564ebd8ea22d3ea5d41ee38f7808afde3708097
Maple <seanafleming2003@gmail.com>
2026-02-11 15:12:26 -0500

    Box declaration fixes

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,4 +1,4 @@
-import { Card, CardActions, CardContent, IconButton, Typography, useTheme } from '@mui/material';
+import { Box, Card, CardActions, CardContent, IconButton, Typography, useTheme } from '@mui/material';
 import LanguageIcon from '@mui/icons-material/Language';
 
 export default function ProjectCard({image, title, children, link}) {
